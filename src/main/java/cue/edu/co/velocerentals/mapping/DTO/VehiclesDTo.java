7bd1e39fe8bf7a051f61352018414bc758227640
Maicol <maicolmontoya323@gmail.com>
2024-04-29 15:38:46 -0500

    Database Data Transfer Objects created

---
+++ src/main/java/cue/edu/co/velocerentals/mapping/DTO/VehiclesDTo.java
@@ -0,0 +1,9 @@
+package cue.edu.co.velocerentals.mapping.DTO;
+
+import cue.edu.co.velocerentals.utils.VehicleStatus;
+import cue.edu.co.velocerentals.utils.VehicleType;
+
+import java.time.Year;
+
+public record VehiclesDTo(Integer vehicle_id, VehicleType type, String make, String model, Year year, double price_per_day, VehicleStatus status) {
+}
