350d9ed35e0d9190168d61540c20e95c6dced25c
Maicol <maicolmontoya323@gmail.com>
2024-04-29 15:07:09 -0500

    Database configuration

---
+++ src/main/java/cue/edu/co/velocerentals/utils/ServiceJdbcException.java
@@ -0,0 +1,13 @@
+package cue.edu.co.velocerentals.utils;
+
+public class ServiceJdbcException extends RuntimeException {
+
+    public ServiceJdbcException(String message) {
+        super(message);
+    }
+
+    public ServiceJdbcException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
