4a5d97cb3df0644d7e108e6fc28a1c56f1131c54
Maicol <maicolmontoya323@gmail.com>
2024-05-12 22:07:12 -0500

    A function was added to obtain the enum character string in any format, whether uppercase, lowercase or alternate

--- src/main/java/cue/edu/co/velocerentals/enums/VehicleStatus.java
+++ src/main/java/cue/edu/co/velocerentals/enums/VehicleStatus.java
@@ -6,7 +6,22 @@ public enum VehicleStatus {
     RENTED("Rented"),
     MAINTENANCE("Maintenance");
 
-    VehicleStatus(String status) {
+    private final String status;
 
+    VehicleStatus(String type) {
+        this.status = type;
+    }
+
+    public String getType() {
+        return this.status;
+    }
+
+    public static VehicleStatus fromString(String type) {
+        for (VehicleStatus v : VehicleStatus.values()) {
+            if (v.status.equalsIgnoreCase(type)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("No constant with text " + type + " found");
     }
 }
--- src/main/java/cue/edu/co/velocerentals/enums/VehicleType.java
+++ src/main/java/cue/edu/co/velocerentals/enums/VehicleType.java
@@ -1,11 +1,25 @@
 package cue.edu.co.velocerentals.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum VehicleType {
 
     CAR("Car"),
     MOTORCYCLE("Motorcycle");
 
-    VehicleType(String type) {
+    private final String type;
 
+    VehicleType(String type) {
+        this.type = type;
+    }
+
+    public static VehicleType fromString(String type) {
+        for (VehicleType v : VehicleType.values()) {
+            if (v.type.equalsIgnoreCase(type)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("No constant with text " + type + " found");
     }
 }
